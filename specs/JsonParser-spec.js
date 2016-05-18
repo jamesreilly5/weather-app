8aadff992ec50b29bbcdddf12b3f233d91f7d79c
James Reilly <reillyj33@gmail.com>
2016-05-18 11:16:14 +0100

    Vary the test data a little more
    This is that so our tests don't give false positives

--- specs/JsonParser-spec.js
+++ specs/JsonParser-spec.js
@@ -6,19 +6,19 @@ describe("ApiClient", function() {
         var validResponseData = JSON.stringify({
             list: [
                 { main: { temp: '1.1', temp_min: '1', temp_max: '1.2'}, weather: [{ description: 'Clear' }] },
-                { main: { temp: '1.1', temp_min: '1', temp_max: '1.2'}, weather: [{ description: 'Clear' }] },
-                { main: { temp: '1.1', temp_min: '1', temp_max: '1.2'}, weather: [{ description: 'Clear' }] },
-                { main: { temp: '1.1', temp_min: '1', temp_max: '1.2'}, weather: [{ description: 'Clear' }] },
-                { main: { temp: '1.1', temp_min: '1', temp_max: '1.2'}, weather: [{ description: 'Clear' }] }
+                { main: { temp: '2.1', temp_min: '2', temp_max: '2.2'}, weather: [{ description: 'Rain' }] },
+                { main: { temp: '3.1', temp_min: '3', temp_max: '3.2'}, weather: [{ description: 'Clear' }] },
+                { main: { temp: '4.1', temp_min: '4', temp_max: '4.2'}, weather: [{ description: 'Sunny' }] },
+                { main: { temp: '5.1', temp_min: '5', temp_max: '5.2'}, weather: [{ description: 'Clear' }] }
             ]
         });
         var expectedOutput = {
             days: [
                 { id: 0, temp: '1.1', maxTemp: '1.2', minTemp: '1', weather: 'Clear' },
-                { id: 1, temp: '1.1', maxTemp: '1.2', minTemp: '1', weather: 'Clear' },
-                { id: 2, temp: '1.1', maxTemp: '1.2', minTemp: '1', weather: 'Clear' },
-                { id: 3, temp: '1.1', maxTemp: '1.2', minTemp: '1', weather: 'Clear' },
-                { id: 4, temp: '1.1', maxTemp: '1.2', minTemp: '1', weather: 'Clear' }
+                { id: 1, temp: '2.1', maxTemp: '2.2', minTemp: '2', weather: 'Rain' },
+                { id: 2, temp: '3.1', maxTemp: '3.2', minTemp: '3', weather: 'Clear' },
+                { id: 3, temp: '4.1', maxTemp: '4.2', minTemp: '4', weather: 'Sunny' },
+                { id: 4, temp: '5.1', maxTemp: '5.2', minTemp: '5', weather: 'Clear' }
             ]
         };
 
